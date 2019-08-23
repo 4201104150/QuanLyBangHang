5a8111e80688fa89581b8502df712dc0770b676d
4201104150 <shenlong436@gmai.com>
2019-08-24 05:00:31 +0700

    fix nữa

--- QLBH/QLBH/Controllers/GuiMailController.cs
+++ QLBH/QLBH/Controllers/GuiMailController.cs
@@ -15,8 +15,8 @@ namespace QLBH.Controllers
         }
         public IActionResult GuiMailQC()
         {
-            string NoiDungMail = "";
-            GoogleMailer.Send("tuannv@hcmue.edu.vn", "Quảng cáo", "Test quản cáo");
+            string NoiDungMail = "<img src=" + "https://gallery.mailchimp.com/4a23862e073675c50fad05c0e/images/171649f1-8312-4053-ac13-8b6951da63cd.jpg"+ "<table style="+"width: 100 % "+"><caption>Liên hệ với chúng tôi</caption><tr><th>Địa chỉ</th><th>Thêm</th></tr><tr><td>ĐHSP 280 ADV</td><td>Email:BanLau@gmail.com</td></tr><tr><td>Facebook</td><td>Youtube</td></tr></table>";
+            GoogleMailer.Send("tuannv@hcmue.edu.vn", "Giảm giá mạnh nhân dịp tựu trường", NoiDungMail);
             return Content("Xong");
         }
     }
